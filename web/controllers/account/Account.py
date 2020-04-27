cb005622f35b66d2aa365fa97ea173cd5467076b
cy <1337103725@qq.com>
2020-04-28 06:07:49 +0800

    '更新商品管理模板'

--- web/controllers/account/Account.py
+++ web/controllers/account/Account.py
@@ -28,57 +28,60 @@ def info():
     resp_data['info'] = info
     return ops_render('account/info.html',resp_data)
 
-@router_account.route('/set',methods=['GET','POST'])
+
+@router_account.route('/set', methods=['GET', 'POST'])
 def set():
-    if request.method == 'GET':
+    if request.method == "GET":
         resp_data = {}
         req = request.args
-        uid = int(req.get('id',0))
+        uid = int(req.get("id", 0))
         info = None
         if uid:
             info = User.query.filter_by(uid=uid).first()
         resp_data['info'] = info
-
-        return ops_render('account/set.html',resp_data)
-    #更新数据库
+        return ops_render('account/set.html', resp_data)
+    # POST  更新数据库
     resp = {
-        'code':200,
-        'msg':'添加成功',
-        'data':{}
+        'code': 200,
+        'msg': "操作成功",
+        'data': {}
     }
-    req = request.args
+    # ajax 发送的数据
+    req = request.values
     id = req['id'] if 'id' in req else 0
     nickname = req['nickname'] if 'nickname' in req else ''
     mobile = req['mobile'] if 'mobile' in req else ''
     email = req['email'] if 'email' in req else ''
     login_name = req['login_name'] if 'login_name' in req else ''
     login_pwd = req['login_pwd'] if 'login_pwd' in req else ''
+
     if nickname is None or len(nickname) < 1:
         resp['code'] = -1
-        resp['msg'] = '请输入符合规范的昵称'
+        resp['msg'] = "请输入符合规范的昵称"
         return jsonify(resp)
     if mobile is None or len(mobile) < 1:
         resp['code'] = -1
-        resp['msg'] = '请输入符合规范的手机号'
+        resp['msg'] = "请输入符合规范的手机号"
         return jsonify(resp)
-    if email is None or len(email) != 11:
+    if email is None or len(email) < 1:
         resp['code'] = -1
-        resp['msg'] = '请输入符合规范的邮箱'
+        resp['msg'] = "请输入符合规范的邮箱"
         return jsonify(resp)
     if login_name is None or len(login_name) < 1:
         resp['code'] = -1
-        resp['msg'] = '请输入符合规范的账号'
+        resp['msg'] = "请输入符合规范的账号"
         return jsonify(resp)
     if login_pwd is None or len(login_pwd) < 6:
         resp['code'] = -1
-        resp['msg'] = '请输入符合规范的密码'
+        resp['msg'] = "请输入符合规范的密码"
         return jsonify(resp)
-    is_exists = User.query.filter(User.login_name == login_name)
-    if is_exists:
+
+    is_exsits = User.query.filter(User.login_name == login_name).first()
+    if is_exsits:
         resp['code'] = -1
-        resp['msg'] = '该登录名已经存在，请更换'
+        resp['msg'] = "用户名已经存在"
         return jsonify(resp)
-    user_info = User.query.filter(uid=id).first()
+    user_info = User.query.filter_by(uid=id).first()
     if user_info:
         model_user = user_info
     else:
@@ -89,7 +92,13 @@ def set():
     model_user.mobile = mobile
     model_user.email = email
     model_user.login_name = login_name
+    if user_info and user_info.uid == 1:
+        resp['code'] = -1
+        resp['msg'] = "该用户为管理员账号，不允许修改"
+        return jsonify(resp)
+    model_user.login_pwd = UserService.generatePwd(login_pwd, model_user.login_salt)
     model_user.updated_time = getCurrentDate()
+
     db.session.add(model_user)
     db.session.commit()
     return jsonify(resp)
