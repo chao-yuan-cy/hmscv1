c506405b5a4fa66e029575beaccc9cf7498ab4eb
cy <1337103725@qq.com>
2020-04-28 12:40:06 +0800

    '搜索初步'

--- web/controllers/account/Account.py
+++ web/controllers/account/Account.py
@@ -1,32 +1,49 @@
-from flask import Blueprint,request,redirect,jsonify
+from flask import Blueprint, request, redirect, jsonify
+
 from application import db
+from common.libs.Helper import ops_render, getCurrentDate
 from common.libs.UrlManager import UrlManager
-from common.libs.Helper import ops_render,getCurrentDate
-from common.models.user import User
-router_account = Blueprint("account_page",__name__)
 from common.libs.user.UserService import UserService
-@router_account.route('/index',)
+from common.models.user import User
+
+router_account = Blueprint("account_page", __name__)
+
+
+@router_account.route('/index')
 def index():
     resp_data = {}
-    list = User.query.all()
+    req = request.values
+    query = User.query
+    print('----------')
+    print(req)
+    print('----------')
+    if 'status' in req and int(req['status']) > -1:
+        query = query.filter(User.status == int(req['status']))
+
+    list = query.all()
     resp_data['list'] = list
+    resp_data['status'] = {
+        '1':'正常',
+        '0':'已删除'
+    }
+    return ops_render('account/index.html', resp_data)
 
-    return ops_render('account/index.html',resp_data)
 
-@router_account.route('/info',)
+@router_account.route('/info')
 def info():
     resp_data = {}
     req = request.args
-    print(type(req.get('id')))
-    uid = int(req.get('id',0))
-    reback_url = UrlManager.buildUrl('/account/index')
+    uid = int(req.get("id", 0))
+    reback_url = UrlManager.buildUrl("/account/index")
     if uid < 1:
         return redirect(reback_url)
+
     info = User.query.filter_by(uid=uid).first()
     if not info:
         return redirect(reback_url)
+
     resp_data['info'] = info
-    return ops_render('account/info.html',resp_data)
+    return ops_render('account/info.html', resp_data)
 
 
 @router_account.route('/set', methods=['GET', 'POST'])
@@ -69,18 +86,19 @@ def set():
         return jsonify(resp)
     if login_name is None or len(login_name) < 1:
         resp['code'] = -1
-        resp['msg'] = "请输入符合规范的账号"
+        resp['msg'] = "请输入符合规范的登录用户名"
         return jsonify(resp)
     if login_pwd is None or len(login_pwd) < 6:
         resp['code'] = -1
         resp['msg'] = "请输入符合规范的密码"
         return jsonify(resp)
 
-    is_exsits = User.query.filter(User.login_name == login_name).first()
+    is_exsits = User.query.filter(User.login_name == login_name, User.uid != id).first()
     if is_exsits:
         resp['code'] = -1
-        resp['msg'] = "用户名已经存在"
+        resp['msg'] = "该登录名已经存在，请更换"
         return jsonify(resp)
+
     user_info = User.query.filter_by(uid=id).first()
     if user_info:
         model_user = user_info
@@ -88,17 +106,54 @@ def set():
         model_user = User()
         model_user.created_time = getCurrentDate()
         model_user.login_salt = UserService.generateSalt()
+
     model_user.nickname = nickname
     model_user.mobile = mobile
     model_user.email = email
     model_user.login_name = login_name
     if user_info and user_info.uid == 1:
         resp['code'] = -1
-        resp['msg'] = "该用户为管理员账号，不允许修改"
+        resp['msg'] = "该用户为Chang，不允许修改"
         return jsonify(resp)
     model_user.login_pwd = UserService.generatePwd(login_pwd, model_user.login_salt)
     model_user.updated_time = getCurrentDate()
 
     db.session.add(model_user)
     db.session.commit()
+    return jsonify(resp)
+
+
+
+@router_account.route('removeOrRecover', methods=['GET', 'POST'])
+def removeOrRecover():
+    resp = {
+        'code':-1,
+        'msg':'操作成功',
+        'data':{}
+    }
+    req = request.values
+    id = req['id'] if 'id' in req else 0
+    acts = req['acts'] if 'acts' in req else ''
+    if acts not in ['remove','recover']:
+        resp['code'] = -1
+        resp['msg'] = '操作有误'
+        return jsonify(resp)
+    if id:
+        user_info = User.query.filter_by(uid=id).first()
+        if not user_info:
+            resp['code'] = -1
+            resp['msg'] = '指定的账号不存在'
+            return jsonify(resp)
+        if user_info and user_info.uid == 1:
+            resp['code'] = -1
+            resp['msg'] = '该用户为超级用户'
+            return jsonify(resp)
+        if acts == 'remove':
+            user_info.status = 0
+        elif acts == 'recover':
+            user_info.status = 1
+        user_info.status = 0
+        user_info.updata_time = getCurrentDate()
+        db.session.add(user_info)
+        db.session.commit()
     return jsonify(resp)
